b1a35f51224dd791f4c3abadb226ee9b3f389a6d
faiz-siddiqh <fasiddiqh72@gmail.com>
2021-03-18 19:03:54 +0530

    Updating final build

--- Automation-Framework-BookMyShow/src/main/App/core/Sports_Util.java
+++ Automation-Framework-BookMyShow/src/main/App/core/Sports_Util.java
@@ -67,7 +67,6 @@ public class Sports_Util {
 			Integer priceOfTheEvent = pair.getValue();
 			row.createCell(0).setCellValue(nameOfTheEvent);
 			row.createCell(1).setCellValue(priceOfTheEvent);
-			System.out.println(nameOfTheEvent + "       " + priceOfTheEvent);
 			count++;
 
 		}
